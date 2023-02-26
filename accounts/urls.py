e3e7d0fc6ba1d8324b3a868f2a7f96b5aa6ee469
rashadmarkz <rashad.markcreative.com>
2023-02-26 22:53:40 +0530

    3.Setuped views and connected urls paths

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("test/",views.test,name="test"),
+    path("signup/",views.SignUpView.as_view(),name = "signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 # Create your views here.
 def test(request):
     return HttpResponse("<h1> OK </h1>")
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
